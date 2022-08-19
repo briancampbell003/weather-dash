f80e454da5e94c08f187afb9fb6bc4cbcbd3a5d1
Brian Campbell <brian.campbell003@gmail.com>
2022-08-18 23:22:29 -0500

    wireframe and search button

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,21 @@
-let weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=41.88&lon=87.63&appid=ea1fe9ddea793eb6b3591286439bab76';
+// let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=ea1fe9ddea793eb6b3591286439bab76';
+let searchBtn = $('#searchBtn');
+let cityInputEl = $('#cityName');
 
-fetch(weatherUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log('City Weather Data \n----------');
-        console.log(data);
-    })
+let searchBtnHandler = function (event) {
+    event.preventDefault();
+
+    let city = cityInputEl.val();
+    console.log(city);
+}
+
+// fetch(weatherUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log('City Weather Data \n----------');
+//         console.log(data);
+//     })
+
+searchBtn.on('click', searchBtnHandler);
