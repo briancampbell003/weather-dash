91077962b0451e85e9ea788f9770836004377bb9
Brian Campbell <brian.campbell003@gmail.com>
2022-08-20 14:55:49 -0500

    display current weather from fetched object

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,50 @@
-// let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=ea1fe9ddea793eb6b3591286439bab76';
 let searchBtn = $('#searchBtn');
-let cityInputEl = $('#cityName');
+let today= dayjs(Date(dayjs())).format('MM/DD/YYYY');
+console.log(today);
 
-let searchBtnHandler = function (event) {
+
+
+let searchBtnHandler = (event) => {
     event.preventDefault();
-
-    let city = cityInputEl.val();
+    let cityInputEl = $('#cityName');
+    let city = cityInputEl.val().trim();
+    let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ea1fe9ddea793eb6b3591286439bab76&units=imperial';
     console.log(city);
+
+    fetch(weatherUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    let weatherData = data;
+                    displayWeatherData(city, weatherData);
+                });
+            } else {
+                // ADD alert modal
+            }
+        })
+
 }
 
-// fetch(weatherUrl)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log('City Weather Data \n----------');
-//         console.log(data);
-//     })
+let displayWeatherData = (city, weatherData) => {
+    console.log(weatherData);
+    let cityNowEl = $('#thisCityNow');
+    cityNowEl.children('h2').text(city + ' (' + today + ')');
+    cityNowEl.children('ul').append( $("<li>" + weatherData.weather.icon + "</li>"));
+    cityNowEl.children('ul').append( $("<li>Temperature: " + weatherData.main.temp + "° F</li>"));
+    cityNowEl.children('ul').append( $("<li>Humidity: " + weatherData.main.humidity + "%"));
+    cityNowEl.children('ul').append( $("<li>Wind: " + weatherData.wind.speed + "mph"));
+    cityNowEl.children('ul').append( $("<li>UV Index: " + "???" + "</li>"));
+
+    
+    
+
+}
+// let displayWeatherData = function(data) {
+//     console.log(data);
+// }
+// function displayWeatherData(data) {
+//     console.log(data);
+// }
 
 searchBtn.on('click', searchBtnHandler);
