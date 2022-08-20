462d2e53878500b521e7cf5635dc956aaa63de93
Brian Campbell <brian.campbell003@gmail.com>
2022-08-20 18:15:42 -0500

    started 5-day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 let searchBtn = $('#searchBtn');
-let today= dayjs(Date(dayjs())).format('MM/DD/YYYY');
+let today= dayjs(Date(dayjs())).format('M/DD/YYYY');
 console.log(today);
 
 
@@ -8,14 +8,15 @@ let searchBtnHandler = (event) => {
     event.preventDefault();
     let cityInputEl = $('#cityName');
     let city = cityInputEl.val().trim();
+    
     let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=ea1fe9ddea793eb6b3591286439bab76&units=imperial';
-    console.log(city);
+    let forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=ea1fe9ddea793eb6b3591286439bab76&units=imperial';
+
 
     fetch(weatherUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
                     let weatherData = data;
                     displayWeatherData(city, weatherData);
                 });
@@ -24,22 +25,51 @@ let searchBtnHandler = (event) => {
             }
         })
 
+    fetch(forecastUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    let forecastData = data;
+                    displayForecastData(city, forecastData);
+                });
+            } else {
+                // ADD alert modal
+            }
+        })
 }
 
 let displayWeatherData = (city, weatherData) => {
     console.log(weatherData);
     let cityNowEl = $('#thisCityNow');
-    cityNowEl.children('h2').text(city + ' (' + today + ')');
-    cityNowEl.children('ul').append( $("<li>" + weatherData.weather.icon + "</li>"));
-    cityNowEl.children('ul').append( $("<li>Temperature: " + weatherData.main.temp + "° F</li>"));
-    cityNowEl.children('ul').append( $("<li>Humidity: " + weatherData.main.humidity + "%"));
-    cityNowEl.children('ul').append( $("<li>Wind: " + weatherData.wind.speed + "mph"));
-    cityNowEl.children('ul').append( $("<li>UV Index: " + "???" + "</li>"));
+    let cityNowIconUrl = 'http://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png';
 
-    
-    
+    cityNowEl.children('h2').text(city + ' (' + today + ')');
+
+    cityNowEl.children('ul').append( $('<li><img src="' + cityNowIconUrl + '"></li>'));
+    cityNowEl.children('ul').append( $('<li>Temperature: ' + weatherData.main.temp + '° F</li>'));
+    cityNowEl.children('ul').append( $('<li>Humidity: ' + weatherData.main.humidity + '%</li>'));
+    cityNowEl.children('ul').append( $('<li>Wind: ' + weatherData.wind.speed + 'mph</li>'));
+    cityNowEl.children('ul').append( $('<li>UV Index: ' + '??' + '</li>'));
 
 }
+
+let displayForecastData = (city, forecastData) => {
+    console.log(forecastData);
+    let cityFiveDay = $('#thisCity5day');
+
+    for (let i = 1; i < 6; i++) {
+       forecastDate = dayjs(today).add(i, 'day');
+       cityFiveDay.children(1).children(i-1).text(dayjs(forecastDate).format('M/DD/YYYY'));
+       console.log(forecastDate);
+        
+    }
+
+}
+
+
+
+
 // let displayWeatherData = function(data) {
 //     console.log(data);
 // }
